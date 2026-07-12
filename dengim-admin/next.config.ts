28d7688ab26646c5b21e5e76df083f8396aa40e5
BedirXaaN <omerbedirhano@gmail.com>
2026-07-13 01:38:40 +0300

    chore: setup github pages deploy for dengim.app

--- dengim-admin/next.config.ts
+++ dengim-admin/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/dengim/admin',
-  assetPrefix: '/dengim/admin/',
   images: {
     unoptimized: true,
     remotePatterns: [
